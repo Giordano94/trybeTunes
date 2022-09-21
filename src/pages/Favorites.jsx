9f442649ccc88945c039e5bf43a7028df22fec1a
Giordano <giordano.mendes94@gmail.com>
2022-09-21 18:48:41 -0300

    requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
+import Loading from './Loading';
 import Header from '../ Header';
+import MusicCard from './MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      favoritesSongs: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setFavorites();
+  }
+
+  setFavorites = async () => {
+    this.setState({ isLoading: true });
+    const getSongs = await getFavoriteSongs();
+    this.setState({ favoritesSongs: getSongs }, () => {
+      this.setState({ isLoading: false });
+    });
+  };
+
   render() {
+    const { favoritesSongs, isLoading } = this.state;
     return (
       <div data-testid="page-favorites">
         Favorites
         <Header />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div>
+            {favoritesSongs.map((song) => (
+              <div key={ song.trackId }>
+                <MusicCard
+                  song={ song }
+                  updateFavoriteSongs={ this.setFavorites }
+                />
+              </div>
+            ))}
+          </div>
+        )}
+        ;
       </div>
     );
   }
